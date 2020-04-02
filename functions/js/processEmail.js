723f62b9a1a4ed6999c0f068b878c3ffd8e33910
jpcordio <joao.cordio@gmail.com>
2020-04-03 00:42:08 +0100

    add captcha

--- functions/js/processEmail.js
+++ functions/js/processEmail.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-    
+
+    //Calling reCaptcha V3
+    grecaptcha.ready(function() {
+
+        grecaptcha.execute('6Lfa8OUUAAAAAG8S9Iu2IObYks_QOjZMKh_S5E3S', {action: 'homepage'}).then(function(token) {
+
+            document.getElementById("token").value = token;
+
+        });
+
+    });
+
+    //Calling Ajax to send email    
     $('#mainForm').submit(function(){
 
         var dados = jQuery( this ).serialize();
@@ -7,13 +19,26 @@ $(document).ready(function(){
         $.ajax({
 
             type: "POST",
-            url: "../functions/php/sendEmail.php",
+            url: "functions/php/sendEmail.php",
             data: dados,
             success: function(data){
                                 
                 $('#showContent').modal('show');
                 $('#mainForm')[0].reset();
             }
+            
+            /*
+               if(data.status == 'success'){
+                   
+                     $('msgEmailSuccess').modal('show');
+                     $('#mainForm')[0].reset();
+                   
+                }else if(data.status == 'error'){
+                    
+                    $('msgEmailFail').modal('show');
+                } 
+                */
+            
 
         });
 
--- functions/php/sendEmail.php
+++ functions/php/sendEmail.php
@@ -1,26 +1,42 @@
 <?php
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-
-
 ini_set('display_errors', 1);
-
 error_reporting(E_ALL);
 
-$from = "joao.paulo@cordio.com.br";
+   
+    $name = $_POST['name'];    
+    $email = $_POST['email'];    
+    $message = $_POST['message'];         
+    $url = "https://www.google.com/recaptcha/api/siteverify";        
+    $data = [
+        'secret' => "6Lfa8OUUAAAAAEYJzcp-4OYS91czV-Qpd_pBjv9Q",
+        'response' => $_POST['token'],
+        'remoteip' => $_SERVER['REMOTE_ADDR']
+    ]; 
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => "Content-type: application/x-www-form-urlencoded\r\n",            
+            'content' => http_build_query($data)
+        )    
+    );  
+          
+    $context = stream_context_create($options);    
+    $response = file_get_contents($url, false, $context);         
+    $res = json_decode($response, true);
+        
+    if($res['success'] >= 0.5){       
+        
+        $from = "joao.paulo@cordio.com.br";
+        $to = "joao.paulo@cordio.com.br";
+        $subject = "Email from the form on Cordio.com.br";
+        $message = "De: " . $name . "\nEmail: " . $email . "\nMensagem: " . $message;
+        $headers = "De: " . $name . "Email: " . $email;
 
-$to = "joao.paulo@cordio.com.br";
+        mail($to, $subject, $message, $headers);
+        
+        echo "SITE ALERT: Email Sent!";
+        
+   } else{ echo "SITE ALERT: reCaptcha Fail!"; }    
 
-$subject = "Email from the form on Cordio.com.br";
-
-$message = "De: " . $name . "\nEmail: " . $email . "\nMensagem: " . $message;
-
-$headers = "De: " . $name . "Email: " . $email;
-
-mail($to, $subject, $message, $headers);
-
-echo "Your e-mail has been sent.";
 
 ?>
